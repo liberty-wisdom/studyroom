7f81c72db51610f53179a805f52c6466452a0303
wisdom <357359654@qq.com>
2020-11-16 19:39:15 +0800

    add findAll()

--- studyroom/src/main/java/com/anyang/mapper/ManagerMapper.java
+++ studyroom/src/main/java/com/anyang/mapper/ManagerMapper.java
@@ -4,7 +4,10 @@ import com.anyang.entity.Manager;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface ManagerMapper {
     Manager findOneManager(@Param("username")String username);
+    List<Manager> findAll();
 }
